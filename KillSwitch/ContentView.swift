006464e5527f0608e7253d60950f21e1e6b691f8
ArtistSamyak <samyak.professional@gmail.com>
2025-08-21 07:29:53 +0530

    Initial Commit

---
+++ KillSwitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KillSwitch
+//
+//  Created by Samyak Pawar on 21/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KillSwitch/KillSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KillSwitchApp.swift
+//  KillSwitch
+//
+//  Created by Samyak Pawar on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct KillSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
